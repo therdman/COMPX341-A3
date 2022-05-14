4d67883c1778d48a776442accd0393aec9b62126
Taylor Herdman <Taylorzaneh@gmail.com>
2022-05-14 22:26:03 +1200

    {commitMessage}

--- assets/pipeline.py
+++ assets/pipeline.py
@@ -1,15 +1,20 @@
 import subprocess
 import sys
+if(len(sys.argv) != 2):
+    print("Please give a commit message, and not other arguments")
+    k = input("press enter to exit")
+    sys.exit(1)
+commitMessage = sys.argv[1]
 subprocess.run("npm install", shell=True)
 try:
     subprocess.run("npm run build", shell=True, check=True)
 except subprocess.CalledProcessError as e:
     print(e.returncode)
     print(e.output)
+    k = input("press enter to exit")
     sys.exit(1)
 subprocess.run("git add -A")
-subprocess.run("git commit -m \"COMPX341-22A-A3 Commiting from CI/CD Pipeline\"", shell=True)
+subprocess.run("git commit -m \"{commitMessage}\"", shell=True)
 subprocess.run("git push origin", shell=True)
 subprocess.run("npm run start", shell=True)
-    
-k = input("----")
+
--- assets/pipeline.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-exec npm install
-exec npm run build
-exec npm run start
